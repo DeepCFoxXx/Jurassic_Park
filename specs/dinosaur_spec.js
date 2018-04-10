ece7d1d61f9b665e15536bbc1c3275da0ee1ad52
DeepCFoxXx <learncode23@gmail.com>
2018-04-10 16:00:27 +0100

    first commit

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var Dinosaur = require('../dinosaur.js');
+
+describe("Dinosaur", function() {
+
+  var plesiosaur;
+
+  beforeEach(function() {
+    plesiosaur = new Dinosaur("plesiosaur", 2);
+  });
+
+});
